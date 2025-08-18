d7adf1a5898173c9e003f4cd82cb823e3042a258
Fantety <fantety@outlook.com>
2025-08-18 17:15:05 +0800

    change api_key

--- bin/start_frameforge.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-TITLE FrameForge
-echo 启动 FrameForge 应用...
-
-"%~dp0rameforge.exe"
-
-pause
--- bin/start_frameforge.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo "启动 FrameForge 应用..."
-
-"$DIR/frameforge"
